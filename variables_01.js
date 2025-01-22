cf0ac22e67cb9c1719014433403c67c2d2e3aaf0
ajitesh68 <ajiteshshukla1234@gmail.com>
2025-01-22 10:50:47 +0530

    updated

---
+++ variables_01.js
@@ -0,0 +1,11 @@
+const accountID=1234
+let accountEmail="ajitesh@245gmail.com"
+accountCity = "Lucknow"
+let accountPassword;
+
+// accountID=3456  // NOT ALLOWED
+
+console.log(accountID)
+
+
+console.table([accountCity,accountEmail,accountID,accountPassword])
