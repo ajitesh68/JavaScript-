a6116897403df819326226c1cade25cd22b942d8
ajitesh68 <ajiteshshukla1234@gmail.com>
2025-01-27 00:21:27 +0530

    updated

---
+++ datatypes_02.js
@@ -0,0 +1,19 @@
+//code should also be readable
+//ECMA and MNM are two documents for datatypes in java script
+
+let name="hitesh"
+let age=18
+
+//number => 2 to power 53
+//bigint
+//string ==> ""
+// Boolean => true/false
+// null => standalone Value 
+// undefined 
+// Symbol => unique 
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
+
+
+
