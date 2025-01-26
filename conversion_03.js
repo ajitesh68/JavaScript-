7faf7f51cf427ae6481138cba08d718e9e0d3921
ajitesh68 <ajiteshshukla1234@gmail.com>
2025-01-27 00:37:58 +0530

    updated

---
+++ conversion_03.js
@@ -0,0 +1,27 @@
+let score = "44"
+
+console.log(typeof(score))
+
+let valueINnum = Number(score)
+console.log(typeof valueINnum)
+console.log(valueINnum);
+
+
+//"33" ->  33
+// "33abs" -> NaN
+// true -> 1 ; false -> 0
+
+
+let isLoggedIN = 1
+let booleanInLoggedIN = Boolean(isLoggedIN)
+console.log(booleanInLoggedIN)
+
+//1 -> true ; 0-> false
+//"" -> false ( if empty string is there then the value will be false)
+// "ajitesh" -> true (if something is written in the string then value will be true)
+
+
+let someNum = 1
+let stringNum = String(someNum)
+console.log(stringNum)
+console.log(typeof(stringNum))
