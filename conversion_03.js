2959511062e11cd18e849acbe13d59965e83d036
ajitesh68 <ajiteshshukla1234@gmail.com>
2025-01-27 18:58:46 +0530

    updated

--- conversion_03.js
+++ conversion_03.js
@@ -25,3 +25,35 @@ let someNum = 1
 let stringNum = String(someNum)
 console.log(stringNum)
 console.log(typeof(stringNum))
+
+//*******************oprerations****************/
+
+let value = 345
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2*2)
+
+
+let str1 ="hello"
+let str2 ="ajitesh"
+
+let str3 =str1 + str2
+
+console.log(str3)
+
+// console.log("1"+2)
+// console.log(2 + 2 + "2")
+// console.log(1 + 2 + "2")//if string is at last then first two will add up then string would conactenate with the number
+// console.log("2" + 2 + 2)//if string is at start then all others after it would be consiered as strings
+
+let num1,num2,num3;
+
+let gameCounter=100;
+// ++gameCounter;
+console.log(++gameCounter);
