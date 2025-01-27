49be07b88c3c72f3ea513d1fc417e769bf586b7e
ajitesh68 <ajiteshshukla1234@gmail.com>
2025-01-27 19:18:56 +0530

    updated

---
+++ comparison_04.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+//TypeScript does not allow to compare two other datatypes
+
+
+console.log("2">0)//JS converted this string into a number automatically
+console.log("02">0)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null<0)
+
+//comparision and equality check are two different things
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined<0)
+
+console.log("2" === 2)
