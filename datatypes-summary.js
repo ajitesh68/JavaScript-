dd3552f98d8b9113924ba6ffe0c5a6a3f3259f8a
ajitesh68 <ajiteshshukla1234@gmail.com>
2025-01-28 23:54:09 +0530

    Updated

---
+++ datatypes-summary.js
@@ -0,0 +1,34 @@
+//primitive
+//7 types : String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp =null
+let userEmail;
+
+const ID = Symbol('123')
+const anotherID = Symbol('123')
+
+console.log(ID == anotherID)
+
+const bigNumber = 2345678457689654n
+
+//Reference (non primitive)
+//Array ,Objects,Functions
+
+const heros = ["shaktiman","naagraj","doga"];
+let myObj ={
+    name:"ajitesh",
+    age : 20
+}
+
+const muFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof anotherID);
+
+
+
