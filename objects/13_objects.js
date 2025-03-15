1892d054ef218aa0f38cad07568d81393fc589b4
ajitesh68 <ajiteshshukla1234@gmail.com>
2025-03-15 19:21:37 +0530

    updated

---
+++ objects/13_objects.js
@@ -0,0 +1,24 @@
+const course = {
+    coursename: "js in hindi",
+    price: "880",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const{courseInstructor: Instructor} = course
+
+// console.log(courseInstructor);
+
+console.log(Instructor);
+
+
+//Destructuring of the object statement
+const navbar= ({company}) => {
+
+}
+
+navbar(company = "hitesh")
+
+
+//jab bhi apna kaam kisi aur ke sir pe daal dena hai toh usi ko API kehte hai
